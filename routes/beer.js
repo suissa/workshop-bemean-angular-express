03dd31e74c2d2d81fcfa43d13560fc0964e2a0d3
Suissa <jnascimento@gmail.com>
2013-09-29 17:20:49 -0300

    Encapsulando a listagem na função listar e chamando via ng-click na view

---
+++ routes/beer.js
@@ -0,0 +1,33 @@
+var _model = require("../models/beer.js");
+
+
+exports.create = function(req, res){
+  _model.create(req, res);
+} 
+
+exports.retrieve = function(req, res){  
+  _model.retrieve(req, res);
+}
+
+exports.list = function(req, res){  
+  console.log("list");
+  _model.find(req, res);
+}
+
+exports.update = function(req, res){
+  _model.update(req, res);
+}
+
+exports.delete = function(req, res){
+  _model.delete(req, res);
+}
+
+
+
+
+
+
+
+
+
+
